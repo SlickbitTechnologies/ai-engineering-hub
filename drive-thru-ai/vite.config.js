7bcbc07d77de49aae7898b817beae80aec0137d9
MLSrinivas70 <80315375+MLSrinivas70@users.noreply.github.com>
2025-04-09 12:29:21 +0530

    Bug fixes

--- drive-thru-ai/vite.config.js
+++ drive-thru-ai/vite.config.js
@@ -4,4 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['national-tough-ghost.ngrok-free.app'],
+    // optionally, you can allow all hosts with:
+    // allowedHosts: 'all'
+  },
 });
+
